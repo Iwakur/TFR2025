1fd52895595866138dc528f9e79efb5d47bd9ae2
unknown <10RyzHli1@enseignementbw.be>
2025-05-22 15:06:08 +0200

    start

---
+++ test.py
@@ -0,0 +1,8 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+print(Fore.BLUE + "1. Guerrier")
+print(Fore.BLUE + "2. Roubard")
+print(Fore.BLUE + "3. Sorcier")
+print(Fore.RED + "\nChoix invalide. Réessayez.")
